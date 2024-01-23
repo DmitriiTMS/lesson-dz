9cb249f32cdaf7da2f04af76a1935b4d7ff9fa8b
DmitriiTMS <dmitrii12_88@mail.ru>
2024-01-23 13:09:23 +0300

    add dz

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php 
+// lesson-variable
+
+#########
+$x = 5;
+echo $x + 3;
+
+#########
+$a = 10;
+$b = 7;
+echo $a / $b;
+
+#########
+$name = 'Алиса';
+$age = 25;
+echo "Меня зовут {$name} и мне {$age} лет";
+
+#########
+$y = 12;
+echo 12 * 2;
+
+#########
+$x1 = 6;
+$y1 = 4;
+echo $x * $y;
+
+#########
+$x2 = 7;
+$y2 = 3;
+echo $x2 + $y2;
+
+#########
+$initialStr = 'TMS';
+echo $initialStr . ' мне нравится программирование';
+
+#########
+$number = 10;
+$number++;
+echo $number;
+
+#########
+$firstStr = 'Привет, ';
+$secondStr = 'мир!';
+echo $firstStr . $secondStr;
+
+#########
+$firstStrNumber = '12';
+$secondNumber = 10;
+echo $firstStrNumber + $secondNumber;
